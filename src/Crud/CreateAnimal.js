d930fbcde009d34a605a9292364424f81fbddc57
Shelby Miller <shelbycrosbee@gmail.com>
2019-06-08 09:15:20 -0600

    dno't remember

--- src/Crud/CreateAnimal.js
+++ src/Crud/CreateAnimal.js
@@ -7,48 +7,48 @@ import TextField from '@material-ui/core/TextField';
 class CreateAnimal extends React.Component {
   constructor(props) {
     super(props)
-    this.state= {
-    newAnimal: {
-      name: "",
-      funFact: ""
+    this.state = {
+      newAnimal: {
+        name: "",
+        funFact: ""
+      }
     }
   }
-}
 
-addNewName = newName => this.setState(
-  {
-    newAnimal: {
-      ...this.state.newAnimal,
-      name: newName
+  addNewName = newName => this.setState(
+    {
+      newAnimal: {
+        ...this.state.newAnimal,
+        name: newName
+      }
     }
-  }
-)
-addNewFact = newFact => this.setState(
-  {
-    newAnimal:{
-      ...this.state.newAnimal,
-      funFact: newFact 
-    }
+  )
+  addNewFact = newFact => this.setState(
+    {
+      newAnimal: {
+        ...this.state.newAnimal,
+        funFact: newFact
+      }
     }
   )
 
   render() {
     return (
-      <form style={{marginLeft: "2em"}}>
-       <label> Add New</label>
-  <TextField
-            name="name"
-            type="text"
-            onChange={e => this.addNewName(e.target.value)}
-            placeholder="animal" />
-       
+      <form style={{ marginLeft: "2em" }}>
+        <label> Add New</label>
+        <TextField
+          name="name"
+          type="text"
+          onChange={e => this.addNewName(e.target.value)}
+          placeholder="  animal" />
 
-  <TextField
-            name="funFact"
-            type="text"
-            onChange={e => this.addNewFact(e.target.value)}
-            placeholder="fun fact" />
-        
+
+        <TextField
+          name="funFact"
+          type="text"
+          onChange={e => this.addNewFact(e.target.value)}
+          placeholder="fun fact" />
+
         <Button onClick={e => this.props.addNewAnimal(e, this.state.newAnimal)} className='buttonColor'> ADD </Button>
       </form>);
   }
